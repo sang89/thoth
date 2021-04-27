123c97a15f8a3d2f8c9a93fdf4b5f6dea768d3e8
Vinh <hangvinh94@gmail.com>
2021-04-26 22:20:13 -0700

    first change to test hooks

--- thoth_frontend/src/App.tsx
+++ thoth_frontend/src/App.tsx
@@ -1,24 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import { Button } from '@material-ui/core';
 
 function App() {
+
+  // 
+  const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+      root: {
+        '& > *': {
+          margin: theme.spacing(1),
+          width: '25ch',
+        },
+      },
+    }),
+  );
+  
+  const [oldInput, setOldInput] = useState('');
+  const [inputText, setInputText] = useState('');
+
+  const classes = useStyles();
+  
+  const handleOnClicked = () => {
+      setOldInput(inputText);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField 
+          id="filled-basic" 
+          label="Question" 
+          variant="outlined"
+          onChange= { event => { setInputText(event.target.value);} } 
+        />
+        <Button 
+          variant="contained"
+          onClick= { () => handleOnClicked()}
         >
-          Learn how to use Thoth
-        </a>
-      </header>
+          Click
+        </Button>
+      </form>
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField 
+          id="filled-basic" 
+          label= {oldInput} 
+          variant="outlined" 
+        />
+      </form>
     </div>
+    
   );
 }
 
