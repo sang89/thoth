f382edbbc64ec6809c1b0482b6a437859e6ba4c5
Vinh <hangvinh94@gmail.com>
2021-05-02 15:55:12 -0700

    adding top bar

--- thoth_frontend/src/App.tsx
+++ thoth_frontend/src/App.tsx
@@ -1,22 +1,29 @@
 import './App.css';
 import ThothButton from './Components/ThothButton';
 import ThothTextField from './Components/ThothTextField';
+import ThothAppBar from './Components/ThothAppBar';
+import Grid from '@material-ui/core/Grid';
 
 function App() {
   return (
     <>
-      <ThothButton
-        color = "primary"
-        variant = "contained" 
-      />
-      <ThothTextField
-        variant = "outlined" 
-        id = "abc"
-      />
-      <ThothTextField
-        variant = "outlined"
-        id = "abc"
-      />
+      <Grid container spacing={3} direction="row" justify="center" alignItems="center">
+        <Grid item xs={12}>
+          <ThothAppBar
+
+          />
+        </Grid>
+        <Grid item xs>
+          <ThothTextField
+            variant = "outlined" 
+            id = "abc"
+          />
+          <ThothButton
+            color = "primary"
+            variant = "contained" 
+          />
+        </Grid>
+      </Grid>
     </>
   );
 }
