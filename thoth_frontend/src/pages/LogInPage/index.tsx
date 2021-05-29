9584b23a365b8ede9d9e01d99626cf7a417652a0
Vinh <hangvinh94@gmail.com>
2021-05-29 15:40:57 -0700

    Move signup form into pages and unify log in page

--- thoth_frontend/src/pages/LogInPage/index.tsx
+++ thoth_frontend/src/pages/LogInPage/index.tsx
@@ -1,17 +1,24 @@
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import { createStyles, Grid, makeStyles, Theme } from "@material-ui/core";
-import { useParams  } from "react-router-dom";
+import { makeStyles, Button, TextField, Link } from "@material-ui/core";
+import { useParams, useHistory  } from "react-router-dom";
 import { useState } from "react";
+import * as routes from "../../routes/constants";
 
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-      root: {
-        margin: theme.spacing(1),
-      },
-    }
-  )
-);
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: theme.spacing(2),
+'& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '300px',
+    },
+'& .MuiButtonBase-root': {
+      margin: theme.spacing(2),
+    },
+  },
+}));
 
 export default function LoginPage() {
 
@@ -21,38 +28,46 @@ export default function LoginPage() {
 
   const [username, setUsername] = useState(defaultUsername);
   const [password, setPassword] = useState('');
-  
+  const [userExisted, setUserExisted] = useState(false);
+
+  const history = useHistory()
+
+  const handleLogIn = () => {
+
+  };
+
+  const handleSignUpLink = () => {
+    history.push(`${routes.SIGN_UP_PAGE}`);
+  }
+
   return (
-    <div className="App">
-      <form className={classes.root}>
-        <Grid container alignItems="center" justify="center">
-          <Grid item xs={12}>
-            <TextField 
-              label="Username" 
-              variant="filled"
-              type="username" 
-              required 
-              value={username}
-              onChange={e=> setUsername(e.target.value)} 
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField 
-              label="Password" 
-              variant="filled" 
-              type="password" 
-              required 
-              value={password}
-              onChange={e => setPassword(e.target.value)} 
-            />
-          </Grid>
-          <Grid item xs={12} >
-            <Button variant="contained" type="button" color="primary">
-              Log in
-            </Button>
-          </Grid>
-        </Grid>
-      </form>
-    </div>
+    <form className={classes.root}>
+      <TextField 
+        label="Username" 
+        variant="filled"
+        type="username" 
+        required 
+        value={username}
+        onChange={e=> setUsername(e.target.value)}
+        error={userExisted}
+        helperText={userExisted ? "Username/password not correct" : ""} 
+      />
+      <TextField 
+        label="Password" 
+        variant="filled" 
+        type="password" 
+        required 
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+        error={userExisted}
+        helperText={userExisted ? "Username/password not correct" : ""} 
+      />
+      <Button variant="contained" color="primary" onClick={handleLogIn}>
+        Log in
+      </Button>
+      <Link href="#" onClick={handleSignUpLink}>
+        Don't have an account yet ? Signup
+      </Link>
+    </form>
   );
 }
